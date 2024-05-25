28bedf0d77c26df936727e1c289cd94646c5a81d
Andrey1914 <anlikajw@gmail.com>
2024-05-25 19:10:28 +0200

    query for data with graphql

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,40 @@
 import * as React from "react";
-
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { StaticImage } from "gatsby-plugin-image";
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
 
-export const Head = () => <Seo title="My Blog Posts" />;
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
+export const Head = () => (
+  <>
+    <meta name="blog-page" content="Blog Page" />
+    <Seo title="My Blog Posts" />
+  </>
+);
 
 export default BlogPage;
